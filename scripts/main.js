fbaa223859f43f403e002696c75529e04a14afe1
Infvmous <alexeyheather@gmail.com>
2021-01-18 06:08:35 +0700

    added header/nav opacity, changed cover bg, cover is ready, centered logo, scroll function

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,60 @@
-const burgerBtn = document.getElementById('burger');
-const navbar = document.getElementById('nav');
-const cover = document.getElementById('cover');
+function smoothScroll(ele, duration) {
+    const target = document.querySelector(ele);
+    const targetPos = target.getBoundingClientRect().top;
+    const startPos = window.pageYOffset;
+    const distance = targetPos - startPos;
+    let startTime = null;
+
+    const animation = (currentTime) => {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const runAnimation = ease(timeElapsed, startPos, distance, duration);
+        window.scrollTo(0, runAnimation);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+
+    const ease = (t, b, c, d) => {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t + b;
+        t--;
+        return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+
+    requestAnimationFrame(animation);
+}
+
+function addScrollEventListener(ele, scrollTo, scrollDuration = 300) {
+    ele.addEventListener('click', () => {
+        smoothScroll(scrollTo, scrollDuration);
+    });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+    // collect dom elements
+    const burgerBtn = document.getElementById('burger');
+    const navbar = document.getElementById('nav');
+    const cover = document.getElementById('cover');
+    const coverArrowDown = document.getElementById('reveal-content');
+    const scrollToMenuBtn = document.getElementById('scroll-to-menu');
+    const main = document.querySelector('.main');
+    const body = document.body;
+
+    // Burger menu
     burgerBtn.addEventListener('click', () => {
         burgerBtn.classList.toggle('active');
         navbar.classList.toggle('active');
-        document.body.classList.toggle('lock');
+        body.classList.toggle('lock');
     });
+
+    // Close navbar menu on main click
+    main.addEventListener('click', () => {
+        if (navbar.classList.contains('active')) {
+            burgerBtn.classList.remove('active');
+            navbar.classList.remove('active');
+            body.classList.remove('lock');
+        }
+    });
+
+    // Scroll about section
+    addScrollEventListener(coverArrowDown, '.about');
 });
