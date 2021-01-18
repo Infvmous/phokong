8cd9c584f65bbd65987e33ba15b3aec1892f3f35
Infvmous <alexeyheather@gmail.com>
2021-01-18 12:30:44 +0700

    changed scroll function

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,23 @@
-function smoothScroll(ele, duration) {
-    const target = document.querySelector(ele);
-    const targetPos = target.getBoundingClientRect().top;
-    const startPos = window.pageYOffset;
-    const distance = targetPos - startPos;
-    let startTime = null;
-
-    const animation = (currentTime) => {
-        if (startTime === null) startTime = currentTime;
-        const timeElapsed = currentTime - startTime;
-        const runAnimation = ease(timeElapsed, startPos, distance, duration);
-        window.scrollTo(0, runAnimation);
-        if (timeElapsed < duration) requestAnimationFrame(animation);
-    };
-
-    const ease = (t, b, c, d) => {
-        t /= d / 2;
-        if (t < 1) return (c / 2) * t * t + b;
-        t--;
-        return (-c / 2) * (t * (t - 2) - 1) + b;
-    };
-
-    requestAnimationFrame(animation);
-}
-
-function addScrollEventListener(ele, scrollTo, scrollDuration = 300) {
+function addScrollEventListener(ele, scrollTo, scrollDuration = 500) {
     ele.addEventListener('click', () => {
         smoothScroll(scrollTo, scrollDuration);
     });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function addScrollEventListener(ele, scrollTo) {
+    ele.addEventListener('click', () => {
+        scrollTo.scrollIntoView({ behavior: 'smooth' });
+    });
+}
+
+function main() {
     // collect dom elements
     const burgerBtn = document.getElementById('burger');
     const navbar = document.getElementById('nav');
-    const cover = document.getElementById('cover');
     const coverArrowDown = document.getElementById('reveal-content');
-    const scrollToMenuBtn = document.getElementById('scroll-to-menu');
-    const main = document.querySelector('.main');
+    const mainContainer = document.querySelector('.main');
     const body = document.body;
+    const aboutSection = document.querySelector('.about');
 
     // Burger menu
     burgerBtn.addEventListener('click', () => {
@@ -47,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Close navbar menu on main click
-    main.addEventListener('click', () => {
+    mainContainer.addEventListener('click', () => {
         if (navbar.classList.contains('active')) {
             burgerBtn.classList.remove('active');
             navbar.classList.remove('active');
@@ -55,6 +35,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Scroll about section
-    addScrollEventListener(coverArrowDown, '.about');
+    addScrollEventListener(coverArrowDown, aboutSection);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    main();
 });
